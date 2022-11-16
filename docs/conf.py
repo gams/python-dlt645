2fa4877eecb7f6f105e702718595d9e52505691f
Stefan 'hr' Berder <stefan@measureofquality.com>
2022-11-16 12:59:14 +0800

    bump version to 0.1.3

--- docs/conf.py
+++ docs/conf.py
@@ -11,9 +11,9 @@ sys.path.insert(0, os.path.abspath(".."))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "python-dlt645"
-copyright = "2022, 0.1.2"
+copyright = "2022, 0.1.3"
 author = "gams monitoring"
-release = "0.1.2"
+release = "0.1.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
